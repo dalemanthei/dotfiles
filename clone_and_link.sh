658ef9e8ff7b16652a1679302a69c7e6cffe6be8
Dale Manthei <dalemanthei@users.noreply.github.com>
2015-12-19 09:10:26 -0600

    First step at clone and link script

---
+++ clone_and_link.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pushd "$HOME" > /dev/null
+
+if [ -d ".dotfiles" ]; then
+	pushd ".dotfiles" > /dev/null
+  # git smart-pull # TODO
+  git pull
+  popd > /dev/null
+else
+	git clone "git://github.com/dalemanthei/dotfiles.git" ".dotfiles"
+fi
+
+echo 'Linking files...'
+
+ls -1d .dotfiles/files/* .dotfiles/files/.* | while read f; do
+	[ "$f" == '.dotfiles/files/.' ] ||
+	[ "$f" == '.dotfiles/files/..' ] ||
+	[ "$f" == '.dotfiles/files/.git' ] ||
+	[ -d "$f" ] || # don't link directories
+	ln -vsf "$f" .
+done
+
+popd > /dev/null
